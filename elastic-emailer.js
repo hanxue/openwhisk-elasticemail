5f3d38523e12212634dc6d8d7279d0215d06bc09
Lee Hanxue <hanxue.developer@gmail.com>
2018-03-15 18:34:16 +0800

    Change parameter names and checking for required parameters
    apiKey -> apikey
    to -> msgTo
    body_text -> bodyText
    
    Added: bodyHtml
    Added: apikey
    
    - Check the following parameters must be set
        - subject
        - msgTo
        - bodyHtml or bodyText
        - apikey

--- elastic-emailer.js
+++ elastic-emailer.js
@@ -4,30 +4,40 @@ const elastic = require('elasticemail')
 function main(args) {
   if (!args.from) {
     return { error: 'Sender is required. Please set the "from" parameter' }
-  } else if (!args.to) {
+  } else if (!args.msgTo) {
     return { error: 'Recipient is required. Please set the "to" parameter' }
-  } else if (!args.subject || !args.body) {
-    return { error: 'Both "subject" and "body" cannot be empty.' }
+  } else if (!args.apikey) {
+    return { error: 'API Key is required. Please set the "apikey" parameter' }
+  } else if (!args.subject) {
+    return { error: 'Subject is required. Please set the "subject" parameter' }
+  } else if (!(args.bodyText || args.bodyHtml)) {
+    return { error: 'Body is required. Please set the "bodyText" or "bodyHtml parameter' }
   }
 
   const client = elastic.createClient({
-    apiKey: '44b6614c-5076-4447-8a1b-93bdbfb54895',
+    apikey: '44b6614c-5076-4447-8a1b-93bdbfb54895',
   })
   const msg = {
     from: args.from,
-    from_name: args.from_name,
-    to: args.to,
+    fromName: args.fromName,
+    msgTo: args.msgTo,
     subject: args.subject,
-    body_text: args.body,
+    bodyText: args.bodyText,
+    bodyHtml: args.bodyHtml,
+    apikey: args.apikey,
   }
 
   client.mailer.send(msg, (err, result) => {
+  client.mailer.send(msg, function (err, result) {
+    console.error('DEBUG: Inside mailer.send, just before checking for err')
     if (err) {
-      return console.error(err)
+      console.error('DEBUG: Just before error from sending email')
+      return { error: `Some problem sending email: ${err}` }
     }
+    console.error('DEBUG: Just before return email successful')
     return { result: `Mail sent succesfully with id: ${result}` }
   })
-  return { error: 'Logically error. Email was not sent, error message uncaught.' }
+  return { error: 'Logic error! Email was not sent, error message uncaught.' }
 }
 
 exports.main = main
